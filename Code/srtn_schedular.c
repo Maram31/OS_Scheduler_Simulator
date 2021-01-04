264be3659555a838789d43f819ad1e3f0693ae79
nadinfromc137 <44551861+nadinfromc137@users.noreply.github.com>
2021-01-04 18:33:39 +0200

    Update srtn_schedular.c

--- Code/srtn_schedular.c
+++ Code/srtn_schedular.c
@@ -1,19 +1,134 @@
 #include "headers.h"
+#include "priority_queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define null 0
+#define MAXCHAR 300
 
-struct Process {
-	int id;
-	int arrivalTime;
-	int priority;
-	bool isRunning;
-	int executionTime;
-    int remainingTime;
-    int waitingTime;
+/*--------------------------------------- FUNCTIONS START HERE------------------------------------------------*/
+//struct Process pcs[10];             // global
 
-    bool isFinished;
-    bool isReady;
-};
+void readFromFileAndFillList(struct LinkedList* list);
+void loadProcess(char str[], struct LinkedList* list);
+void addNodeToLikedlistEnd(struct LinkedList* list, struct Process processToAdd);
+bool startsWith(const char *a, const char *b);
 
 
+void readFromFileAndFillList(struct LinkedList* list)
+{
+    FILE * pFile;
+    char str[MAXCHAR];
+    pFile = fopen("processes.txt", "r");
+
+    if (pFile == NULL){
+        printf("Could not open file");
+        return;
+    }
+
+    long long index = 0;
+    while (fgets(str, MAXCHAR, pFile) != NULL)
+    {
+        if(!startsWith(str,"#"))
+        {
+            loadProcess(str,list);
+        }
+        index ++;
+    }
+    fclose(pFile);
+}
+
+bool startsWith(const char *a, const char *b)
+{
+   if(strncmp(a, b, strlen(b)) == 0) return 1;
+   return 0;
+}
+
+void addNodeToLikedlistEnd(struct LinkedList* list, struct Process processToAdd)
+{
+    struct Node * nodeToAdd = (struct Node *) malloc(sizeof(struct Node));  //create new node and assign the process to it.
+    nodeToAdd->processInfo = processToAdd;
+    nodeToAdd->next = NULL;
+
+    if(list->head == NULL)
+    {
+        list->head = nodeToAdd;
+        list->tail = nodeToAdd;
+    }
+    else
+    {
+        list->tail->next = nodeToAdd;
+        nodeToAdd->previous = list->tail;
+        list->tail = list->tail->next;
+    }
+
+    list->size++;
+}
+
+void loadProcess(char str[], struct LinkedList* list)
+{
+    struct Process newProcess;
+    int init_size = strlen(str);
+	char delim[] = "\t";
+	char *ptr = strtok(str, delim);
+    
+    int i = 0;
+	while(ptr != NULL)
+	{
+
+        char *my_string = malloc(MAXCHAR);
+        strcpy(my_string, ptr);
+        if(i == 0)
+        {
+            newProcess.id = atoi(my_string);
+            i ++;
+        }
+        else if (i == 1)
+        {
+            newProcess.arrivalTime = atoi(my_string);
+            i ++;
+        }
+        else if (i == 2)
+        {
+            newProcess.executionTime = atoi(my_string);
+            i ++;
+        }
+        else if (i == 3)
+        {
+            newProcess.priority = atoi(my_string);
+            i ++;
+        }
+
+		ptr = strtok(NULL, delim);
+        free(my_string);
+	}
+
+    addNodeToLikedlistEnd(list, newProcess);
+}
+
+
+/*---------------------------------------MAIN STARTS HERE------------------------------------------------*/
+
+
+
+int main()
+{
+    
+
+
+
+
+}
+
+
+
+/*---------------------------------------ARCHIVED CODE START HERE------------------------------------------------*/
+
+
+
+
+// PREVIOUS EDIT, KEEP IF NEEDED
+/*
 void sort(int noofpcs)
 {
      for (int i = 0; i < noofpcs-1; i++)
@@ -32,12 +147,12 @@ void sort(int noofpcs)
     }
 }
 
-struct Process pcs[10];             // global
 
-int main()
-{
-    /*Nadin: THIS FILE IS A GENRAL DEMO OF WHAT SRTN SCHEDULAR IS LIKE, NOT THE FINAL OR WORKING IMPLEMENTATION*/
+//PREVIOUSLY IN MAIN, WHEN IMPLEMENTED AS ARRAY
 
+
+/*THIS FILE IS A GENRAL DEMO OF WHAT SRTN SCHEDULAR IS LIKE, NOT THE FINAL OR WORKING IMPLEMENTATION*/
+    /*
     int process_id, arrival_time, run_time, num_processes;
 
     // Assume we have a DS array with number of processes = num_processes
@@ -58,7 +173,7 @@ int main()
     }
 
 
-    /*Bubble sort according to execution/running time*/
+    //Bubble sort according to execution/running time
 
     for (int i = 0; i < num_processes-1; i++)
     {
@@ -75,7 +190,7 @@ int main()
         
     }
 
-    /*Simulation starts*/
+    //Simulation starts
     int index_of_running;
     while(1)
     {
@@ -108,10 +223,5 @@ int main()
 
 
     }
-    
-    
-
-
-
-
-}
+  
+  */
