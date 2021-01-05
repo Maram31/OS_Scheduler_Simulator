c65849e5dec95bcce942f517122d5c952d1bf164
ososito <ahmadosgalal@gmail.com>
2021-01-05 14:48:51 +0200

    RR missing sleep and termination

--- Code/process.c
+++ Code/process.c
@@ -9,17 +9,23 @@ int main(int agrc, char * argv[])
     int pid = atoi(argv[1]);
     int arrival_time = atoi(argv[2]);
     int running_time = atoi(argv[3]);
+    //printf("Ok\n");
+    printf("Inside process current time is %d\n", getClk());
     printf("Created Process ID: %d\tArrival time: %d\tRunning Time: %d\n", pid, arrival_time, running_time);
     //TODO it needs to get the remaining time from somewhere
     int start_time = getClk();
     remainingtime = running_time;
+    int prev_time = getClk();
     //remainingtime -= 1;
-    while (getClk() - start_time != running_time)
+    while (remainingtime > 0)//getClk() - start_time != running_time)
     {
+        while(getClk() - prev_time == 0);
+        prev_time = getClk();
+        remainingtime --;
     }
     //sleep(1);
     int x = getClk();
-    printf("Process %d finished execution\tcurrent time is %d\nTerminating...\n", pid, x);
+    printf("Process %d finished execution\tcurrent time is %d\nTerminating...\n", pid, prev_time);
 
     while(1);
 
